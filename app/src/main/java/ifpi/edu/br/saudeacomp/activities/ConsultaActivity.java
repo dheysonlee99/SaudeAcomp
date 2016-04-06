f1d1c717e54b57a742df16498480e47fbd534e0e
dheysonlee99 <dheysonlee@gmail.com>
2016-04-06 10:43:17 -0300

    Colocando DataPicker em formulario de consulta e exame

--- app/src/main/java/ifpi/edu/br/saudeacomp/activities/ConsultaActivity.java
+++ app/src/main/java/ifpi/edu/br/saudeacomp/activities/ConsultaActivity.java
@@ -1,14 +1,21 @@
 package ifpi.edu.br.saudeacomp.activities;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+
+import java.util.Calendar;
 
 import ifpi.edu.br.saudeacomp.R;
 import ifpi.edu.br.saudeacomp.dao.DBHelper;
@@ -23,6 +30,9 @@ public class ConsultaActivity extends AppCompatActivity {
     private int paciente_id;
     private DBHelper db;
     private EditText  data;
+    private int ano, mes, dia;
+    private String dataFormat = "0/0/0";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +46,44 @@ public class ConsultaActivity extends AppCompatActivity {
         db = new DBHelper(this);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        data.setInputType(InputType.TYPE_NULL);
+        data.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    datePicker(v);
+                }
+            }
+        });
+    }
+    public void datePicker(View v){
+        hideKeyboard(v);
+        DatePickerDialog.OnDateSetListener odsl = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                ano = year;
+                mes = monthOfYear + 1;
+                dia = dayOfMonth;
+                dataFormat = String.format("%02d/%02d/%04d", dia, mes, ano);
+                data.setText(dataFormat);
+                data.setSelection(dataFormat.length());
+            }
+        };
+        final Calendar c = Calendar.getInstance();
+        DatePickerDialog dlg = new DatePickerDialog(ConsultaActivity.this, odsl, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
+        if (dataFormat != "0/0/0") {
+            dlg.updateDate(ano, mes - 1, dia);
+        }
+        dlg.show();
+    }
 
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
     public void agendaClick(View elementoClicado){
 
 
--- app/src/main/java/ifpi/edu/br/saudeacomp/activities/ExameActivity.java
+++ app/src/main/java/ifpi/edu/br/saudeacomp/activities/ExameActivity.java
@@ -1,13 +1,20 @@
 package ifpi.edu.br.saudeacomp.activities;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 import ifpi.edu.br.saudeacomp.R;
 import ifpi.edu.br.saudeacomp.dao.DBHelper;
@@ -22,16 +29,66 @@ public class ExameActivity extends AppCompatActivity {
 
     int paciente_id;
     private DBHelper db;
+    private EditText  data;
+    private int ano, mes, dia;
+    private String dataFormat = "0/0/0";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exame);
+
+        data = (EditText)findViewById(R.id.data_exames);
         paciente_id = getIntent().getIntExtra("paciente_id", 0);
         Toast.makeText(ExameActivity.this, "ID Recebido: " + paciente_id, Toast.LENGTH_SHORT).show();
 
         db = new DBHelper(this);
 
     }
+
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        data.setInputType(InputType.TYPE_NULL);
+        data.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    datePicker(v);
+                }
+            }
+        });
+    }
+    public void datePicker(View v){
+        hideKeyboard(v);
+        DatePickerDialog.OnDateSetListener odsl = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                ano = year;
+                mes = monthOfYear + 1;
+                dia = dayOfMonth;
+                dataFormat = String.format("%02d/%02d/%04d", dia, mes, ano);
+                data.setText(dataFormat);
+                data.setSelection(dataFormat.length());
+            }
+        };
+        final Calendar c = Calendar.getInstance();
+        DatePickerDialog dlg = new DatePickerDialog(ExameActivity.this, odsl, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
+        if (dataFormat != "0/0/0") {
+            dlg.updateDate(ano, mes - 1, dia);
+        }
+        dlg.show();
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+
+
 
 
 
